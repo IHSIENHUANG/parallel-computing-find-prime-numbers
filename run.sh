e096816214c634e45d3e749dd0f302ccb567319a
I-HSIEN HUANG <isytkr34@gmail.com>
2017-11-17 13:18:23 -0800

    scheduling jobfile submission

--- run.sh
+++ run.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-echo "test"
 #make all
-qsub part1.job
-qsub part2.job
+
+jobid=$(qsub part1_node1.job| cut -d '.' -f 1)
+
+jobid=$(qsub -W depend=afterok:${jobid} part1_node2.job | cut -d '.' -f 1 )
+jobid=$(qsub -W depend=afterok:${jobid} part1_node4.job | cut -d '.' -f 1 )
+jobid=$(qsub -W depend=afterok:${jobid} part1_node8.job | cut -d '.' -f 1 )
+jobid=$(qsub -W depend=afterok:${jobid} part2_node1.job | cut -d '.' -f 1 )
+jobid=$(qsub -W depend=afterok:${jobid} part2_node2.job | cut -d '.' -f 1 )
+jobid=$(qsub -W depend=afterok:${jobid} part2_node4.job | cut -d '.' -f 1 )
+jobid=$(qsub -W depend=afterok:${jobid} part2_node8.job | cut -d '.' -f 1 )
+jobid=$(qsub -W depend=afterok:${jobid} part3_node1.job | cut -d '.' -f 1 )
+jobid=$(qsub -W depend=afterok:${jobid} part3_node2.job | cut -d '.' -f 1 )
+jobid=$(qsub -W depend=afterok:${jobid} part3_node4.job | cut -d '.' -f 1 )
+qsub -W depend=afterok:${jobid} part3_node8.job | cut -d '.' -f 1
+
+
+
